74f3aa0043f63b71080a5f40d380afbcbbbb3584
risa06-hub <risamhjn@gmail.com>
2026-04-19 23:41:58 +0530

    Update kalman.py

--- kalman.py
+++ kalman.py
@@ -7,7 +7,7 @@ class KalmanPhaseTracker:
     self.dt= dt
     self.x= np.array([0,0]) #initial state
     self.P= np.eye(2)*1      #uncertainity
-    self.Q= np.array([[process_noise,0],[0,process_noise]])    #process nosie covariance 
+    self.Q= np.array([[process_noise_phase,0],[0,process_noise_phase]])    #process nosie covariance 
     self.R= np.array([[measurement_noise]])  #measurement noise covariance
     self.F= np.array([[1,dt],[0,1]])    #state transition matrix( phase = phase+ freq*dt)
     self.H= np.array([[1,0]])           #meaurement matrix (only phase)
@@ -29,6 +29,45 @@ class KalmanPhaseTracker:
 
     #updated state
     self.x= self.x + K @ y
-    self.P= (np.eye(2) - K @ slef.H) @ self.P
-    
+    self.P= (np.eye(2) - K @ self.H) @ self.P
+
+def smooth(self, measurement):
+  #Rauch_tung Smooting for Kalman tracker
+  n=len(measurement)
+  x_filtered=[]
+  P_filtered=[]
+  x_predicted=[]
+  P_predicted=[]
+
+  x_predicted.append(self.x.copy())
+  P_predicted.append(self,P.copy())
+
+  print("Forward pass")
+  for i,z in enumerate(measurement):
+    #predict step
+    pred_phase = self.predict()  #to update x and P
+    x_predicted.append(self.x.copy())
+    P_predicted.append(self.P.copy())
+
+    self.update(np.array([z]))
+    x_filtered.append(self.x.copy())
+    P_filtered.append(self.P.copy())
+
+  #backward smoothing pass
+  print("Backward pass")
+  x_smooth=np.zeros((len(x_filtered),2))
+  P_smooth=np.zeros((len(x_filtered),2,2))
+
+  #last smooth=last filtered
+  x_smooth[-1] = x_filtered[-1]
+  P_smooth[-1] = P_filtered[-1]
+
+  #backward recursion
+  for k in range(len(x_filtered)-2,-1,-1):
+    P_pred_inv=np.linalg.inv(P_predicted[k+1])
+    C_k= P_filtered[k] @ self.F.T @ P_pred_inv
+    x_smooth[k]= x_filtered + c_k @ (x_smooth[k+1]-x_predicted[k+1]) @ C_k.T
+    return x_smooth, P_smooth
+
+
     
